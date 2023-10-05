4b5cc7a52c53eea1bba0f72cda20efdcb370f924
zl52074 <737218582@qq.com>
2023-10-06 05:43:44 +0800

    整合spring-cloud-gateway

---
+++ gulimall-gateway/src/test/java/com/zl52074/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zl52074.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
