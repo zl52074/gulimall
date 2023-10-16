7779eb182f5dfd5978ffe013844d0b96a197921e
zl52074 <737218582@qq.com>
2023-10-16 18:02:40 +0800

    项目结构细节优化

--- gulimall-third-party/src/main/java/com/zl52074/gulimall/thirdparty/GulimallThirdPartyApplication.java
+++ gulimall-third-party/src/main/java/com/zl52074/gulimall/thirdparty/GulimallThirdPartyApplication.java
@@ -3,14 +3,15 @@ package com.zl52074.gulimall.thirdparty;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @EnableFeignClients
 @EnableDiscoveryClient
-@SpringBootApplication
 @EnableEncryptableProperties
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class GulimallThirdPartyApplication {
     public static void main(String[] args) {
         System.setProperty("jasypt.encryptor.privateKeyFormat","PEM");
