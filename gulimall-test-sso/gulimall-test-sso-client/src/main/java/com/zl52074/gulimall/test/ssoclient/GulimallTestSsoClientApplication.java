32b8b0219f2344cdd1d935a0526cb86968c4396a
zl52074 <737218582@qq.com>
2023-10-26 14:10:47 +0800

    sso单点登录测试

---
+++ gulimall-test-sso/gulimall-test-sso-client/src/main/java/com/zl52074/gulimall/test/ssoclient/GulimallTestSsoClientApplication.java
@@ -0,0 +1,16 @@
+package com.zl52074.gulimall.test.ssoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: zl52074
+ * @time: 2023/10/26 13:36
+ */
+@SpringBootApplication
+public class GulimallTestSsoClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallTestSsoClientApplication.class, args);
+    }
+}
---
+++ gulimall-test-sso/gulimall-test-sso-server/src/main/java/com/zl52074/gulimall/test/ssoserver/GulimallTestSsoServerApplication.java
@@ -0,0 +1,16 @@
+package com.zl52074.gulimall.test.ssoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: zl52074
+ * @time: 2023/10/26 13:36
+ */
+@SpringBootApplication
+public class GulimallTestSsoServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallTestSsoServerApplication.class, args);
+    }
+}
